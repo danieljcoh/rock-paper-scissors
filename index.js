06a4b6a133e57acf10ebc7c3fd45ceeddc10be4c
Daniel <danieljcoh@gmail.com>
2023-05-06 01:51:02 -0400

    added images, and updates
    - Finalized MVP
    - Added images

--- index.js
+++ index.js
@@ -16,38 +16,56 @@
 //ARRAY
 let images = [
     "imgs/paper_img.png",
-    "img/rock_img.png",
-    "img/paper_img"
+    "imgs/rock_img.png",
+    "imgs/scissors_img.png",
+    "imgs/hi_img.png"
 ]
 
 
 // VARIABLES
 let player_choice = document.getElementById("player_choice");
 let btn = document.getElementById("fight_btn");
-let player_image = document.getElementById("player_img")
+let player_image = document.getElementById("player_battlefield_img")
 let computer_image = document.getElementById("computer_img")
+let computer_choice = document.getElementById("computer_choice")
+
+
+// FUNCTIONS
+function computer__RPS_choice() {
+    let random_computer_choice = Math.floor(Math.random() * 3)
+
+    computer_image.innerHTML = `<img id="computer_img" src=${images[random_computer_choice]} alt="a test image">`
+    if (random_computer_choice === 0) {
+        computer_choice.innerHTML = `<h3>Paper</h3>`
+    } else if (random_computer_choice === 1) {
+        computer_choice.innerHTML = `<h3>Rock</h3>`
+    } else if (random_computer_choice === 2) {
+        computer_choice.innerHTML = `<h3>Scissors</h3>`
+    }
+}
+
 
 // EVENT LISTENERS
 btn.addEventListener("click", function() {
-
 
     let player_result = player_choice.value;
     let player_result_lowercase = player_result.toLowerCase();
 
     if (player_result_lowercase === "rock") {
-        //IMG
         player_image.innerHTML = `<img id="player_img" src=${images[1]} alt="a test image">`
+        computer__RPS_choice()
     }
     else if (player_result_lowercase === "paper") {
-        //IMG
+        player_image.innerHTML = `<img id="player_img" src=${images[0]} alt="a test image">`
+        computer__RPS_choice()
     }
     else if (player_result_lowercase === "scissors") {
-        //IMG
+        player_image.innerHTML = `<img id="player_img" src=${images[2]} alt="a test image">`
+        computer__RPS_choice()
     }
     else {
         alert("That's an invalid response!")
     }
 
-    alert(player_result_lowercase)
 })
 
